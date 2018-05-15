744fad6e0b0ca5d4a034cba30fb3364b3398fc19
DanielKell <danielkell.ubc@gmail.com>
2018-05-14 20:22:47 -0700

    Dummy dashboard image added

--- src/components/WelcomePage/WelcomePage.js
+++ src/components/WelcomePage/WelcomePage.js
@@ -9,7 +9,9 @@ const WelcomePage = ({ user, onSignOut }) => {
       <h3 className="welcome-text">
         Hi {user.fullName}, welcome to your Alfred Dashboard!
       </h3>
-      <div className="welcome-page-main" />
+      <div className="welcome-page-main" >
+      <img alt="Dashboard" src="http://www.visualmining.com/wp-content/uploads/2012/10/Retail-Operational-Overview-Dashboard.jpg" />
+      </div>
       <a onClick={onSignOut}>
         <RaisedButton>Sign out</RaisedButton>
       </a>
